8bccf2c2a43460f81b37429499e7e51dd2c291c1
Yura Chuchola <smhfanda@gmail.com>
2016-08-04 11:45:53 +0300

    1. Adde ParamsBuilder class for construct complex parameters string. 2. Modified README.md file.

---
+++ pandoc-slave/pandoc-slave/paramsbuilder.cpp
@@ -0,0 +1,73 @@
+#include "paramsbuilder.h"
+
+PandocSlave::ParamsBuilder::ParamsBuilder(QObject *parent) :
+    QObject(parent)
+  , mError("")
+{
+    initializeKeys();
+
+    mMetaEnumForFormats = QMetaEnum::fromType<PandocFormat>();
+    mMetaEnumForKeys = QMetaEnum::fromType<PandocKeys>();
+}
+
+QString PandocSlave::ParamsBuilder::fromFormat(PandocFormat format) const
+{
+    return QString(mMetaEnumForFormats.valueToKey(format)).toLower();
+}
+
+QString PandocSlave::ParamsBuilder::fromKeys(PandocKeys key) const
+{
+    QString keyEnumStr = QString(mMetaEnumForKeys.valueToKey(key)).toLower();
+    return mKeys[keyEnumStr];
+}
+
+void PandocSlave::ParamsBuilder::addParam(PandocKeys key, PandocFormat format)
+{
+    if (key == PandocKeys::empty)
+    {
+        mError = "Not allowed parameters combination: "
+                "PandocKeys::empty and PandocFormat::" +
+                fromFormat(format);
+    }
+    mParams << fromKeys(key) << fromFormat(format);
+}
+
+void PandocSlave::ParamsBuilder::addParam(PandocKeys key, const QString &value)
+{
+    if (key != PandocKeys::empty)
+    {
+        mParams << fromKeys(key);
+    }
+    if (value != "")
+    {
+        mParams << value;
+    }
+}
+
+QStringList PandocSlave::ParamsBuilder::params() const
+{
+    return mParams;
+}
+
+void PandocSlave::ParamsBuilder::setParams(QStringList params)
+{
+    mParams = params;
+}
+
+QString PandocSlave::ParamsBuilder::toString() const
+{
+    return mParams.join(" ");
+}
+
+QString PandocSlave::ParamsBuilder::error() const
+{
+    return mError;
+}
+
+void PandocSlave::ParamsBuilder::initializeKeys()
+{
+    mKeys["from"] = "-f";
+    mKeys["to"] = "-t";
+    mKeys["standalone"] = "-s";
+    mKeys["empty"] = "";
+}
